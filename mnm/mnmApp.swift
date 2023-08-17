568c1d3e91e075ac0d44a43fa5d53b942db85ba7
Rana Mavi <ranaamavii@gmail.com>
2023-08-17 13:13:47 +0300

    Initial Commit

---
+++ mnm/mnmApp.swift
@@ -0,0 +1,17 @@
+//
+//  mnmApp.swift
+//  mnm
+//
+//  Created by Rana Mavi on 17.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct mnmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
