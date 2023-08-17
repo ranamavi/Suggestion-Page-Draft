d5330647000f0097ac66f16fe1c81a869d27c72c
Klara Bragesjö Björkén <klara.bjorken@gmail.com>
2023-08-17 20:49:59 +0200

    Created variables to store all the text that will be displayed. Still need to change the text and incorporate the variables into the quiz results

--- mnm/ContentView.swift
+++ mnm/ContentView.swift
@@ -12,6 +12,22 @@ struct ContentView: View {
         
         NavigationStack{
             
+            var feynmanTechniqueDescription = "The Feynman Technique is that you learn by explaining something in a way that even children would understand. This means that the explanation should be in simple words and very concise. Firstly, pick a topic. Then, pretend to teach it to a child. Afterwards, refine and review your understanding and lastly organize the notes and review them regularly."
+            
+            var leitnerSystemDescription = "The Leitner System is that you make flashcards and put them in 3 boxes. Each card starts in box 1. Go through all the flashcards one box at a time starting with box 1. The cards are moved to the next box when they're remembered correctly and are moved to previos box or box 1 (if i originally was there) when they're not remember correctly."
+            
+            var pomodoroMethodDescription = "Pomodoro Method is that you do 25 minutes of deeply focused work followed by a 5 minute break. The cycle can be repeated multiple times, especially if a lot of studying needs to be done. The work to break ratio can also be changed depending on your own preferences."
+            
+            var secondBrainDescription = "Second Brain is that you create a new brain for yourself in either a digital or physical organizer. Firstly, decide your subject and then organize it to make the information easily accessible. Then, summarize the information. Remember to make them understandable for your future self! Lastly, do something with the information to not forget it as easily."
+            
+            var feynmanTechniqueWhoShouldUseIt = "If you want a deep understanding of something you should use Feynman. And if you’re not able to work on quite place (like on a trip or on a picnic) you can use the Feynman Technique."
+            
+            var leitnerSystemWhoShouldUseIt = "The Leitner System can be used if you want to remember things for a long time, since it includes spaced repetition. It can also be used for practically any subject."
+            
+            var pomodoroMethodWhoShoulduseIt = "The Pomodoro Method can be used to break down large tasks into smaller, more manageable ones. It’s also good to use if you’re not good at working for long periods of time."
+            
+            var secondBrainWhoShouldUseIt = "Second Brain can be used to prevent you from forgetting things if you’re trying to store a lot of information in your brain at the same time. It can also be used to avoid burnout, stress, etc. It’s also good for final exams, etc. that require a longer study period."
+            
             ZStack{
                 Color("awesomeb")
                     .ignoresSafeArea()
@@ -27,13 +43,13 @@ struct ContentView: View {
                              .frame(width: 360, height: 320)
                             
                         VStack{
-                            Text("Feynman Technique")
+                            Text("Feynman Technique") //change into the var that describes what result you got on the quiz
                                 .font(.title)
                                 .fontWeight(.bold)
                                 .foregroundColor(Color.white)
                                 .padding()
                             
-                            Text("The Feynman Technique is that you learn something by explaining it in a way that you would do to a child. Firstly you should Choose a topic. Then teach it to a child (or pretend that you are). Then review and refine your understanding. And lastly organize the notes and review them.")
+                            Text("Second Brain is that you create a new brain for yourself in either a digital or physical organizer. Firstly, decide your subject and then organize it to make the information easily accessible. Then, summarize the information. Remember to make them understandable for your future self! Lastly, do something with the information to not forget it as easily.") //change into to the var with the WhoShouldUseIt at the end
                                 .fontWeight(.medium)
                                 .foregroundColor(Color.white)
                                 .multilineTextAlignment(.center)
@@ -44,12 +60,11 @@ struct ContentView: View {
                     .padding()
                     
                     ZStack{
-                        
                         RoundedRectangle(cornerRadius: 30)
                              .frame(width: 360, height: 270)
                         
                         VStack{
-                            Text("Who should use the Feynman Technique:")
+                            Text("Who should use the Feynman Technique?")
                                 .font(.title)
                                 .fontWeight(.bold)
                                 .foregroundColor(Color.white)
