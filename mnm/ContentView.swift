fa0dbd6fcc271489c43a261fd860c958ddc9eb72
Rana Mavi <ranaamavii@gmail.com>
2023-08-17 20:32:40 +0300

    Suggestion page

--- mnm/ContentView.swift
+++ mnm/ContentView.swift
@@ -9,13 +9,88 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+        
+        NavigationStack{
+            
+            ZStack{
+                Color("awesomeb")
+                    .ignoresSafeArea()
+                
+                VStack{
+                    Spacer()
+                    Text("Suggestion Page")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                    
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 30)
+                             .frame(width: 360, height: 320)
+                            
+                        VStack{
+                            Text("Feynman Technique")
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.white)
+                                .padding()
+                            
+                            Text("The Feynman Technique is based on explaining something you learned as if you were explaining it to a child. Firstly you should Choose a topic. Then teach it to a child (or pretend that you are). Then review and refine your understanding. And lastly organize the notes and review them.")
+                                .fontWeight(.medium)
+                                .foregroundColor(Color.white)
+                                .multilineTextAlignment(.center)
+                                .padding()
+                            
+                        }
+                    }
+                    .padding()
+                    
+                    ZStack{
+                        
+                        RoundedRectangle(cornerRadius: 30)
+                             .frame(width: 360, height: 270)
+                        
+                        VStack{
+                            Text("Who should use the Feynman Technique:")
+                                .font(.title)
+                                .fontWeight(.bold)
+                                .foregroundColor(Color.white)
+                            
+                            Text("If you want a deep understanding of something you should use Feynman. And if you’re not able to work on quite place (like on a trip or on a picnic) you can use the Feynman Technique.")
+                                .fontWeight(.medium)
+                                .foregroundColor(Color.white)
+                                .multilineTextAlignment(.center)
+                                .padding()
+                        }
+                        .padding()
+
+                    }
+                    .padding()
+                                        
+                    
+                    NavigationLink(destination: secondview()) {
+                        
+                        Text("Learn More About Feynman➡️")
+                            .fontWeight(.bold)
+                            .foregroundColor(Color.black)
+                    }
+                    
+
+                    
+                    
+                }//end of vstack
+     
+                
+            }//end of zstack
+
+           
+            
+            
+                
+            
+        } //navigation stack
+
+
+        
+        
     }
 }
 
---
+++ mnm/secondview.swift
@@ -0,0 +1,54 @@
+//
+//  secondview.swift
+//  mnm
+//
+//  Created by Rana Mavi on 17.08.2023.
+//
+
+import SwiftUI
+
+struct secondview: View {
+    var body: some View {
+        ZStack{
+            Color("awesomeb")
+                .ignoresSafeArea()
+            
+            RoundedRectangle(cornerRadius: 30)
+                .frame(width: 365, height: 750)
+            
+            VStack{
+                
+                Text("More About Feynman Texhnique!")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                
+                Text("1- Choose a topic: Write down stuff that you’re interested about (or that you’re studying.")
+                    .foregroundColor(Color.white)
+                    .padding()
+                
+                Text("2- Teach it to a child (or pretend that you are): Simplify everything by using words that even children would understand.")
+                    .foregroundColor(Color.white)
+                    .padding()
+                
+                Text("3- Review and refine your understanding: Write down your explanation and ensure that it is simple and concise. If it isn’t, then revise by going through the material again and repeating step 2 and 3 until the explanation is simple.")
+                    .foregroundColor(Color.white)
+                    .padding()
+                
+                Text("4- Organize the notes and review them: Teach the concept to someone (a friend, partner, etc.) and see how efficient the explanation was by taking notes on what in the explanation that was unclear, and so on. Improve the explanation after the feedback. Lastly, organize all your notes on every concept that you have in one place.")
+                    .foregroundColor(Color.white)
+                    .padding()
+            }.padding()
+
+            
+        }//en of zstack for color
+        
+    }
+}
+
+struct secondview_Previews: PreviewProvider {
+    static var previews: some View {
+        secondview()
+    }
+}
